fd1912fd0ebda4347bcaef08bd0271fc15e94c04
MatteoMenin <113881461+Matteo-Menin@users.noreply.github.com>
2023-05-10 11:33:29 +0200

    cambiato menu.sh

--- menu.sh
+++ menu.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
-#!/bin/bash
+
 # Define the menu options
-options=("Ricerca con Nome" "Ricerca File Txt" "Ricerca file per tipo" "Esci")
+options=("Option 1" "Option 2" "Option 3" "Quit")
 
 # Define a function to handle the user's selection
 handle_selection() {
     case $1 in
         1) # Option 1
-            echo "Eseguo il codice dell'opzione 1..."
-	     read -p "Inserisci il nome del file che vuoi cercare : " nfile	
-		sudo find . -name "$nfile"	    
-	    ;;
-        2) # Opzione 2
-            echo "Eseguo il codice dell'opzione 2..."
-            # Paste your code for Option 2 here
+            read -p "Inserisci il nome del file/cartella che vuoi cercare : " nfile	
+		    sudo find . -name "$nfile"	
+            echo "Eseguo il codice dell'opzione 1..."  
             ;;
-        3) # Opzione 3
-            echo "Eseguo il codice dell'opzione 3..."
+        2) # Option 2
+            read -p "Inserisci il nome  del file txt che vuoi cercare: " namefile
+            sudo find . -type f -name "$namefile.txt"
+            echo "Eseguo il codice dell'opzione 2..."
+            ;;
+        3) # Option 3
+            echo "Executing code for Option 3..."
             # Paste your code for Option 3 here
             ;;
-        4) # Uscita
-            echo "Uscita in corso..."
+        4) # Quit
+            echo "Exiting..."
             exit
             ;;
-        *) # Uscita
+        *) # Invalid option
             echo "Invalid option. Please try again."
             ;;
     esac
