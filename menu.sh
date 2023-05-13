6cbabe6105ae51cc8eabdc5f5e8300316985450b
MatteoDanese <115141603+MatteoDanese@users.noreply.github.com>
2023-05-13 10:52:48 +0200

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -18,7 +18,34 @@ handle_selection() {
             ;;
         3) # Option 3
             echo "Executing code for Option 3..."
-            # Paste your code for Option 3 here
+            sudo find . -type f -name ".estensioneFile" 5)mkdir a b c d e f g h i l m n o p q r s t u v w x y z
+            mv a?* A?* a
+            mv b?* B?* b
+            mv c?* C?* c
+            mv d?* D?* d
+            mv e?* E?* e
+            mv f?* F?* f
+            mv g?* G?* g
+            mv h?* H?* h
+            mv i?* I?* i
+            mv j?* J?* j
+            mv k?* K?* k
+            mv l?* L?* l
+            mv m?* M?* m 
+            mv n?* N?* n 
+            mv o?* O?* o 
+            mv p?* P?* p 
+            mv q?* Q?* q 
+            mv r?* R?* r
+            mv s?* S?* s 
+            mv t?* T?* t 
+            mv u?* U?* u 
+            mv v?* V?* v
+            mv w?* W?* w 
+            mv x?* X?* x 
+            mv y?* Y?* y
+            mv z?* Z?* z
+            
             ;;
         4) # Quit
             echo "Exiting..."
