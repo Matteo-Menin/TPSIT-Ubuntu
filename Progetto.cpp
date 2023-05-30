c9ad70355015e98f82f24a9e4cb4719e1b5f67d8
MatteoMenin <113881461+Matteo-Menin@users.noreply.github.com>
2023-05-30 10:12:27 +0200

    Inserite Funzioni 5-6

--- Progetto.cpp
+++ Progetto.cpp
@@ -5,9 +5,6 @@
 #include <vector>
 #include <ctype.h>
 
-using namespace std;
-
-
 void move(char letter, std::string format, int option) {
     char upperletter = toupper(letter);
     std::stringstream ss;
@@ -21,101 +18,129 @@ void move(char letter, std::string format, int option) {
     }
     system(ss.str().c_str());
 }
-
 void movethreaded(int option) {
-    vector<char> alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
-    vector<string> formats = { "doc", "docx", "docm", "dot", "dotx", "dotm", "rtf", "odt", "txt", "xls", "xlsx", "xlsm", "xlsb", "xlt", "xltx", "xltm", "csv", "ods", "ppt", "pptx", "pptm", "pot", "potx", "potm", "ppsx", "ppsm", "odp", "psd", "psb", "ai", "eps", "pdf", "indd", "indt", "indb", "tif", "tiff", "mp4", "mov", "avi", "wmv", "mkv", "flv", "mpg", "mpeg", "3gp", "m4v", "webm", "vob", "ts", "m2ts", "mts", "ogv", "rmvb", "mp3", "wav", "flac", "aac", "m4a", "ogg", "wma", "aiff", "ape", "alac", "dsd", "pcm" };
-    vector<thread> threads;
+    std::vector<char> alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+    std::vector<std::string> formats = { "doc", "docx", "docm", "dot", "dotx", "dotm", "rtf", "odt", "txt", "xls", "xlsx", "xlsm", "xlsb", "xlt", "xltx", "xltm", "csv", "ods", "ppt", "pptx", "pptm", "pot", "potx", "potm", "ppsx", "ppsm", "odp", "psd", "psb", "ai", "eps", "pdf", "indd", "indt", "indb", "tif", "tiff", "mp4", "mov", "avi", "wmv", "mkv", "flv", "mpg", "mpeg", "3gp", "m4v", "webm", "vob", "ts", "m2ts", "mts", "ogv", "rmvb", "mp3", "wav", "flac", "aac", "m4a", "ogg", "wma", "aiff", "ape", "alac", "dsd", "pcm" };
+    std::vector<std::thread> threads;
     switch (option) {
     case 5:
         for (int i = 0; i < alphabet.size(); i++) {
-            threads.push_back(thread(move, alphabet[i], "filler", option));
+            threads.push_back(std::thread(move, alphabet[i], "filler", option));
         }
         break;
     case 6:
         for (int i = 0; i < formats.size(); i++) {
-            threads.push_back(thread(move, 'f', formats[i], option));
+            threads.push_back(std::thread(move, 'f', formats[i], option));
         }
     }
     for (auto& th : threads) {
         th.join();
     }
 }
+void size(int maxsize, std::string dirss) {
+    std::stringstream size10mss;
+    std::stringstream size100mss;
+    std::stringstream size1gss;
+    std::stringstream sizeover1gss;
+    switch (maxsize) {
+    case 10:
+        size10mss << dirss << "mkdir 10M" << " && " << "find -size -10M -exec mv {} 10M/ \\";
+        system(size10mss.str().c_str());
+        break;
+    case 100:
+        size100mss << dirss << "mkdir 10M" << " && " << "find -size -100M -exec mv {} 100M/ \\";
+        system(size100mss.str().c_str());
+        break;
+    case 1000:
+        size1gss << dirss << "mkdir 1G" << " && " << "find -size -1000M -exec mv {} 1G/ \\";
+        system(size1gss.str().c_str());
+        break;
+    case 1001:
+        sizeover1gss << dirss << "mkdir over1G" << " && " << "find -size +1000M -exec mv {} over1G/ \\";
+        system(sizeover1gss.str().c_str());
+        break;
+    }
+}
 
-
-int menu(string dirssderiv) {
-    
+int menu(std::string dirssderiv) {
     int option;
-    
-    string name;
-    string format;
-    string directory;
-    stringstream txtss;
-    stringstream namess;
-    stringstream dirss;
-    stringstream mkdiralphabetss;
-    stringstream mkdirformatss;
-    vector<thread> threadssize;
-    
-    cout << "1) Selezionare su quale directory eseguire il programma" << endl;
-    cout << "2) Ricerca di un file/cartella dato il nome" << endl;
-    cout << "3) Ricerca di tutti i file .txt che contengono una data parola" << endl;
-    cout << "4) Ricerca di file per tipo" << endl;
-    cout << "5) Creazione di cartelle che dividano i file in ordine alfabetico" <<endl;
-    cout << "6) Creazione di cartelle che dividano i file per tipo" <<endl;
-    cout << "7) Creazione di cartelle che dividano i file per dimensione (categorie: sotto 10 MB, sotto 100 MB, sotto 1GB, file pi˘ grandi)" <<endl;
-    cout << "8) Quit" << endl;
-    cout << endl;
-    cout << "Inserisci il numero dell'operazione che vuoi eseguire: ";
-    cin >> option;
-    
+    std::string name;
+    std::string format;
+    std::string directory;
+    std::stringstream txtss;
+    std::stringstream namess;
+    std::stringstream dirss;
+    std::stringstream mkdiralphabetss;
+    std::stringstream mkdirformatss;
+    std::vector<std::thread> threadssize;
+    std::cout << "1) Selezionare su quale directory eseguire il programma" << std::endl;
+    std::cout << "2) Ricerca di un file/cartella dato il nome" << std::endl;
+    std::cout << "3) Ricerca di tutti i file .txt che contengono una data parola" << std::endl;
+    std::cout << "4) Ricerca di file per tipo" << std::endl;
+    std::cout << "5) Creazione di cartelle che dividano i file in ordine alfabetico" << std::endl;
+    std::cout << "6) Creazione di cartelle che dividano i file per tipo" << std::endl;
+    std::cout << "7) Creazione di cartelle che dividano i file per dimensione (categorie: sotto 10 MB, sotto 100 MB, sotto 1GB, file pi� grandi)" << std::endl;
+    std::cout << "8) Quit" << std::endl;
+    std::cout << std::endl;
+    std::cout << "Inserisci il numero dell'operazione che vuoi eseguire: ";
+    std::cin >> option;
     switch (option) {
-    case 1:          
-        cout << "Inserisci la directory su cui vuoi lavorare: ";
-        cin >> directory;
+    case 1:
+
+        std::cout << "Inserisci la directory su cui vuoi lavorare: ";
+        std::cin >> directory;
         dirss << "cd " << directory << " && ";
-        cout << "Eseguo l'operazione " << option << "..." << endl;
+        std::cout << "Eseguo l'operazione " << option << "..." << std::endl;
         break;
     case 2:
-        cout << "Inserisci il nome del file che vuoi cercare: ";
-        cin >> name;
+        std::cout << "Inserisci il nome del file che vuoi cercare: ";
+        std::cin >> name;
         namess << dirssderiv << "find . -name " << name;
-        cout << "Eseguo l'operazione " << option << "..." << std;
+        std::cout << "Eseguo l'operazione " << option << "..." << std::endl;
         system(namess.str().c_str());
         break;
     case 3:
-        cout << "Inserisci il nome del file txt che vuoi cercare: ";
-        cin >> name;
+        std::cout << "Inserisci il nome del file txt che vuoi cercare: ";
+        std::cin >> name;
         txtss << dirssderiv << "find . -type f -name " << name;
-        cout << "Eseguo l'operazione " << option << "..." << endl;
+        std::cout << "Eseguo l'operazione " << option << "..." << std::endl;
         system(txtss.str().c_str());
         break;
     case 4:
-        cout << "Inserisci il formato del file che vuoi cercare: ";
-        cin >> format;
+        std::cout << "Inserisci il formato del file che vuoi cercare: ";
+        std::cin >> format;
         namess << dirssderiv << "find . -type f -name *." << format;
-        cout << "Eseguo l'operazione " << option << "..." << endl;
+        std::cout << "Eseguo l'operazione " << option << "..." << std::endl;
         break;
     case 5:
-            mkdiralphabetss << dirssderiv << "mkdir a b c d e f g h i j k l m n o p q r s t u v w x y z";
-            system(mkdiralphabetss.str().c_str());
-            cout << "Eseguo l'operazione " << option << "..." <<endl;
-            movethreaded(option);
+        mkdiralphabetss << dirssderiv << "mkdir a b c d e f g h i j k l m n o p q r s t u v w x y z";
+        system(mkdiralphabetss.str().c_str());
+        std::cout << "Eseguo l'operazione " << option << "..." << std::endl;
+        movethreaded(option);
     case 6:
-            // Metti il codice qui
+        mkdirformatss << dirssderiv << "mkdir doc docx docm dot dotx dotm rtf odt txt xls xlsx xlsm xlsb xlt xltx xltm csv ods ppt pptx pptm pot potx potm ppsx ppsm odp psd psb ai eps pdf indd indt indb tif tiff mp4 mov avi wmv mkv flv mpg mpeg 3gp m4v webm vob ts m2ts mts ogv rmvb mp3 wav flac aac m4a ogg wma aiff ape alac dsd pcm";
+        system(mkdirformatss.str().c_str());
+        std::cout << "Eseguo l'operazione " << option << "..." << std::endl;
+        movethreaded(option);
+        break;
+
     case 7:
-            // Metti il codice qui
+        threadssize.push_back(std::thread(size, 10, dirssderiv));
+        threadssize.push_back(std::thread(size, 100, dirssderiv));
+        threadssize.push_back(std::thread(size, 1000, dirssderiv));
+        threadssize.push_back(std::thread(size, 1001, dirssderiv));
+        for (auto& th : threadssize) {
+            th.join();
+        }
+
+        break;
     case 8:
-        cout << "Exiting..." << endl;
+        std::cout << "Exiting..." << std::endl;
         break;
     }
-    if (option == 1) { 
-        menu(dirss.str().c_str()); 
-    }
+    if (option == 1) { menu(dirss.str().c_str()); }
     return 0;
 }
-
-
 int main() {
     menu("$PWD");
 }
